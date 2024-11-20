8a1d495bbee1244263d41720d2d141bcba392eae
yuzi <kitty841002@gmail.com>
2024-11-20 16:13:00 +0800

    建立路徑

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 建立一個開發中的路徑
+  // process.env.NODE_ENV=>取的node環境中的變數
+  // 如果目前的路徑等於產品的路徑，就執行倉庫的路徑
+  base: process.env.NODE_ENV === 'production' ? '/myTodoDemo/' : '/',
   plugins: [react()],
 })
